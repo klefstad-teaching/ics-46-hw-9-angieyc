ad5ff0077d1a3586caf0ecf831d802e8aa6cac7d
Angie Cheng <angieyc@uci.edu>
2025-03-08 23:30:16 +0000

    finished ladder, haven't really tested edge cases

--- src/ladder_main.cpp
+++ src/ladder_main.cpp
@@ -9,7 +9,7 @@ using namespace std;
 int main() {
     set<string> word_list;
     load_words(word_list, "words.txt");
-    vector<string> word_ladder = generate_word_ladder("code", "data", word_list);
+    vector<string> word_ladder = generate_word_ladder("angie", "curls", word_list);
     print_word_ladder(word_ladder);
     return 0;
 }
